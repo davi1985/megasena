c4effd402039052f4465a0ff38985f1cf1027824
Davi Silva <32596296+davi1985@users.noreply.github.com>
2018-09-21 00:28:32 -0300

    Update megasena.js

--- megasena.js
+++ megasena.js
@@ -1,22 +1,21 @@
-<script>
-	//Simulando uma megasena(sorteando 6 número num range de 0 a 60)
-	let sorteio;
-	let mega = new Array();
 
-	let i = 0;
-	//sorteando os números
-	while (i < 6) {
-		sorteio = Math.ceil(Math.random()*60);
-		if (mega.indexOf(sorteio) < 0) {
-			mega.push(sorteio);
-			i++;
-		}
+//Simulando uma megasena(sorteando 6 número num range de 0 a 60)
+let sorteio;
+let mega = new Array();
+
+let i = 0;
+//sorteando os números
+while (i < 6) {
+	sorteio = Math.ceil(Math.random()*60);
+	if (mega.indexOf(sorteio) < 0) {
+		mega.push(sorteio);
+		i++;
 	}
-	//ordenando os numeros sorteados
-	function orderNumber(a,b) {
-		return a - b;
-	}
-	mega.sort(orderNumber);
-	console.log(mega);
-</script>
- 
+}
+//ordenando os numeros sorteados
+function orderNumber(a,b) {
+	return a - b;
+}
+mega.sort(orderNumber);
+console.log(mega);
+
